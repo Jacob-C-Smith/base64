c172b0b9e7262b4dee5a5edcd0deeb54e51842c3
Jacob Smith <jacobsmithsinbox@gmail.com>
2023-10-02 21:15:47 -0600

    Updated example program

--- main.c
+++ main.c
@@ -15,14 +15,22 @@ int main ( int argc, const char *argv[] )
     (void) argv;
 
     // Initialized data
-    const char hello_world[14] = "Hello, World!";
-    char hello_world_base64[512] = { 0 };
+    const char hello_world[13+1] = "Hello, World!";
+    const char hello_world_base64[18+1] = "SGVsbG8sIFdvcmxkIQ";
+    char hello_world_base64_out[512] = { 0 };
+    char hello_world_out[512] = { 0 };
 
     // Encode the string in base64
-    base64_encode(hello_world, strlen(hello_world), hello_world_base64);
+    base64_encode(hello_world, strlen(hello_world), hello_world_base64_out);
 
     // Print the encoded string
-    printf("\"%s\" encoded in base64 is \"%s\"\n", hello_world, hello_world_base64);
+    printf("\"%s\" encoded in base64 is \"%s\"\n", hello_world, hello_world_base64_out);
+
+    // Decode the base64 to a string
+    base64_decode(hello_world_base64, strlen(hello_world_base64), hello_world_out);
+
+    // Print the decoded string
+    printf("\"%s\" decoded from base64 is \"%s\"\n", hello_world_base64, hello_world_out);
 
     // Success
     return EXIT_SUCCESS;
