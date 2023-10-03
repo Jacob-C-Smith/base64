66bb5920a841035d432e8807c3d2f594dbaa5287
Jacob Smith <jacobsmithsinbox@gmail.com>
2023-10-02 23:47:39 -0600

    Implemented decoder

--- base64.c
+++ base64.c
@@ -10,16 +10,34 @@
 #include <base64/base64.h>
 
 // Constant data
-static const char base_64_characters[64] = {
-                                      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-                                      'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-                                      'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-                                      'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
-                                      'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-                                      'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-                                      'w', 'x', 'y', 'z', '0', '1', '2', '3',
-                                      '4', '5', '6', '7', '8', '9', '+', '/'
-                                    };
+static const char base_64_encoding_characters[64] = {
+                                                        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', // 0   - 7
+                                                        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', // 8   - 15
+                                                        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', // 16  - 23
+                                                        'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', // 24  - 31
+                                                        'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', // 32  - 39
+                                                        'o', 'p', 'q', 'r', 's', 't', 'u', 'v', // 40  - 47
+                                                        'w', 'x', 'y', 'z', '0', '1', '2', '3', // 48  - 55
+                                                        '4', '5', '6', '7', '8', '9', '+', '/'  // 56  - 63
+                                                    };
+static const char base_64_decoding_characters[128] = {
+                                                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 0   - 7
+                                                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 8   - 15
+                                                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 16  - 23
+                                                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 24  - 31
+                                                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 32  - 39
+                                                        0x00, 0x00, 0x00, 0x3E, 0x00, 0x00, 0x00, 0x3F, // 40  - 47
+                                                        0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, // 48  - 55
+                                                        0x3C, 0x3D, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 56  - 63
+                                                        0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, // 64  - 71
+                                                        0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, // 72  - 79
+                                                        0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, // 80  - 87
+                                                        0x17, 0x18, 0x19, 0x00, 0x00, 0x00, 0x00, 0x00, // 88  - 95
+                                                        0x00, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, 0x20, // 96  - 103
+                                                        0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, // 104 - 111
+                                                        0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F, 0x30, // 112 - 119
+                                                        0x31, 0x32, 0x33, 0x00, 0x00, 0x00, 0x00, 0x00  // 120 - 127
+                                                    };
 
 int base64_encode ( const void *const p_data, size_t len, char *const p_output )
 {
@@ -41,10 +59,10 @@ int base64_encode ( const void *const p_data, size_t len, char *const p_output )
                              ( (i+2 < len) ? ((unsigned char*)p_data)[i+2] : 0);
 
         // Write the part to the output
-        p_output[j]   = base_64_characters[(part >> 3 * 6) & 0x3F];
-        p_output[j+1] = base_64_characters[(part >> 2 * 6) & 0x3F];
-        p_output[j+2] = base_64_characters[(part >> 1 * 6) & 0x3F];
-        p_output[j+3] = base_64_characters[(part >> 0 * 6) & 0x3F];
+        p_output[j]   = base_64_encoding_characters[(part >> 3 * 6) & 0x3F];
+        p_output[j+1] = base_64_encoding_characters[(part >> 2 * 6) & 0x3F];
+        p_output[j+2] = base_64_encoding_characters[(part >> 1 * 6) & 0x3F];
+        p_output[j+3] = base_64_encoding_characters[(part >> 0 * 6) & 0x3F];
     }
 
     // Insert a null terminator in the correct position
@@ -84,7 +102,21 @@ int base64_decode ( const char *const p_data, size_t len, void *const p_output )
     if ( p_data   == (void *) 0 ) goto no_data;
     if ( p_output == (void *) 0 ) goto no_output;
 
-    (void) len;
+    // Iterate through len bytes of p_data
+    for (size_t i = 0, j = 0; i < (( (len+5) * 3 ) / 4 ); i+=4, j+=3)
+    {
+
+        // Initialized data
+        unsigned long long part = (base_64_decoding_characters[p_data[i+0]] << 3 * 6) + 
+                                  (base_64_decoding_characters[p_data[i+1]] << 2 * 6) +
+                                  (base_64_decoding_characters[p_data[i+2]] << 1 * 6) +
+                                  (base_64_decoding_characters[p_data[i+3]] << 0 * 6);
+
+        // Write the part to the output
+        ((unsigned char *const)p_output)[j+0] = (part >> 2 * 8) & 0xFF;
+        ((unsigned char *const)p_output)[j+1] = (part >> 1 * 8) & 0xFF;
+        ((unsigned char *const)p_output)[j+2] = (part >> 0 * 8) & 0xFF;
+    }
 
     // Success
     return 1;
