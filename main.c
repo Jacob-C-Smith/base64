424d7a277a44270ff7d1e7f657ecb6c29a8ab110
Jacob Smith <jacobsmithsinbox@gmail.com>
2023-10-02 23:47:53 -0600

    Updated example program

--- main.c
+++ main.c
@@ -15,23 +15,41 @@ int main ( int argc, const char *argv[] )
     (void) argv;
 
     // Initialized data
-    const char hello_world[13+1] = "Hello, World!";
-    const char hello_world_base64[18+1] = "SGVsbG8sIFdvcmxkIQ";
-    char hello_world_base64_out[512] = { 0 };
-    char hello_world_out[512] = { 0 };
+    const char hello_world            [13+1] = "Hello, World!";
+    char       hello_world_base64_out [512]  = { 0 };
+    char       hello_world_out        [512]  = { 0 };
 
     // Encode the string in base64
-    base64_encode(hello_world, strlen(hello_world), hello_world_base64_out);
+    if ( base64_encode(hello_world, strlen(hello_world), hello_world_base64_out) == 0 ) goto failed_to_encode;
 
     // Print the encoded string
-    printf("\"%s\" encoded in base64 is \"%s\"\n", hello_world, hello_world_base64_out);
+    (void)printf("\"%s\" encoded in base64 is \"%s\"\n", hello_world, hello_world_base64_out);
 
     // Decode the base64 to a string
-    base64_decode(hello_world_base64, strlen(hello_world_base64), hello_world_out);
+    if ( base64_decode(hello_world_base64_out, strlen(hello_world_base64_out), hello_world_out) == 0 ) goto failed_to_decode;
 
     // Print the decoded string
-    printf("\"%s\" decoded from base64 is \"%s\"\n", hello_world_base64, hello_world_out);
+    (void)printf("\"%s\" decoded from base64 is \"%s\"\n", hello_world_base64_out, hello_world_out);
 
     // Success
     return EXIT_SUCCESS;
+
+    // Error handling
+    {
+        failed_to_encode:
+
+            // Print an error message to standard out
+            printf("Failed to encode data!\n");
+
+            // Error
+            return EXIT_FAILURE;
+
+        failed_to_decode:
+        
+            // Print an error message to standard out
+            printf("Failed to decode data!\n");
+
+            // Error
+            return EXIT_FAILURE;
+    }
 }
